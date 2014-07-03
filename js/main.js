489a32ac991a1f1ac0443ecda247948f3a3b53c8
jackpopp <jack.popp@hotmail.com>
2014-07-03 18:40:21 +0100

    Refactored into more dynamic solution. Just add trigger with data-note attribute referencing the note.

--- js/main.js
+++ js/main.js
@@ -1,61 +1,20 @@
 $(document).ready(function(){
+
+    // loop through all triggers, grab the note attribute and map into a array
+    sounds = $('.trigger').map(function(){
+        return $(this).data('note')
+    })
+
     //loads in sounds to ionsound
     $.ionSound({
-        sounds: [
-            "1Asharp",
-            "2C",
-            "3D",
-            "4Dsharp",
-            "5F",
-            "6G",
-            "7A",
-            "8Bflat",
-            "9Csharp",
-            "10D",
-
-        ],
+        sounds: sounds,
         path: "sounds/",
         multiPlay: true,
         volume: "1.0"
     });
     //attaches sounds to triggers
-    $("#note1").mouseenter(function(){
-        $.ionSound.play("1Asharp");
+    $(".trigger").mouseenter(function(){
+        $.ionSound.play($(this).data('note'));
     });
 
-    $("#note2").mouseenter(function(){
-        $.ionSound.play("2C");
-    });
-
-    $("#note3").mouseenter(function(){
-        $.ionSound.play("3D");
-    });
-
-    $("#note4").mouseenter(function(){
-        $.ionSound.play("4Dsharp");
-    });
-
-    $("#note5").mouseenter(function(){
-        $.ionSound.play("5F");
-    });
-
-    $("#note6").mouseenter(function(){
-        $.ionSound.play("6G");
-    });
-
-    $("#note7").mouseenter(function(){
-        $.ionSound.play("7A");
-    });
-
-    $("#note8").mouseenter(function(){
-        $.ionSound.play("8Bflat");
-    });
-
-    $("#note9").mouseenter(function(){
-        $.ionSound.play("9Csharp");
-    });
-
-    $("#note10").mouseenter(function(){
-        $.ionSound.play("10D");
-    });
 });
