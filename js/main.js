7b09cc86167eae27657deef9e1d2056cd6cc5739
sgoudie <hello@samuelgoudie.co.uk>
2014-07-24 14:33:52 +0100

    Delay Effects Trial
    Main reason for moving over Web Audio is fx loops. Looking to see if
    it’s worth it. It is!

--- js/main.js
+++ js/main.js
@@ -17,4 +17,30 @@ $(document).ready(function(){
         $.ionSound.play($(this).data('note'));
     });
 
+    (function(){
+        var context = new AudioContext();
+        var audioElement = $('audio')[0];
+
+        audioElement.addEventListener('play', function(){
+            var source = context.createMediaElementSource(audioElement);
+
+            var delay = context.createDelay();
+            delay.delayTime.value = 0.5;
+
+            var feedback = context.createGain();
+            feedback.gain.value = 0.8;
+
+            var filter = context.createBiquadFilter();
+            filter.frequency.value = 1500;
+
+            delay.connect(feedback);
+            feedback.connect(filter);
+            filter.connect(delay);
+
+            source.connect(delay);
+            source.connect(context.destination);
+            filter.connect(context.destination);
+        });
+    })();
+
 });
